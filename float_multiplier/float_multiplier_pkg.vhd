e629850572cba1aaeaaf0297bab8d996bdcc5b5d
jari <jaarhonka@yahoo.com>
2022-03-24 20:00:51 +0200

    fix multiplier

--- float_multiplier/float_multiplier_pkg.vhd
+++ float_multiplier/float_multiplier_pkg.vhd
@@ -21,11 +21,6 @@ package float_multiplier_pkg is
         shift_register    : std_logic_vector(2 downto 0);
     end record;
 
-    -- constant init_pipeline : std_logic_vector(2 downto 0) := "000";
-    -- constant zero_exponent : t_exponent := (others => '0');
-    -- constant init_exponent_pipeline : exponent_array(0 downto 0) := (0=>zero_exponent);
-
-    -- constant init_float_multiplier : float_multiplier_record := (zero, zero, zero, init_pipeline, init_pipeline, init_exponent_pipeline, (others => '0'));
     constant init_float_multiplier : float_multiplier_record := (zero, zero, zero, '0', (others => '0'),(others => '0'), (others => '0'));
 ------------------------------------------------------------------------
     procedure create_float_multiplier (
@@ -88,15 +83,13 @@ package body float_multiplier_pkg is
     ) 
     is
 
-        -- alias exponent_pipeline is float_multiplier_object.exponent_pipeline;
-        alias shift_register                is float_multiplier_object.shift_register;
-        -- alias sign_pipeline                is float_multiplier_object.sign_pipeline;
-        alias mantissa_multiplication_result is float_multiplier_object.mantissa_multiplication_result;
-        alias left                          is float_multiplier_object.left;
-        alias right                         is float_multiplier_object.right;
-        alias result                         is float_multiplier_object.result;
-        alias sign is float_multiplier_object.sign;
-        alias exponent is float_multiplier_object.exponent;
+        alias shift_register                 is  float_multiplier_object.shift_register;
+        alias mantissa_multiplication_result is  float_multiplier_object.mantissa_multiplication_result;
+        alias left                           is  float_multiplier_object.left;
+        alias right                          is  float_multiplier_object.right;
+        alias result                         is  float_multiplier_object.result;
+        alias sign                           is  float_multiplier_object.sign;
+        alias exponent                       is  float_multiplier_object.exponent;
     begin
 
         shift_register                    <= shift_register(shift_register'left-1 downto 0) & '0';
