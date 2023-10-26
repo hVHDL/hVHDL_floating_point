05b07333c9bd599bc35b94fcce66f04cc9688ecd
Jari Honkanen <jaarhonka@yahoo.com>
2023-10-26 06:51:23 +0300

    add float to integer converter to ghdl_compile

--- ghdl_compile_vhdl_float.bat
+++ ghdl_compile_vhdl_float.bat
@@ -15,6 +15,7 @@ ghdl -a --ieee=synopsys --std=08 %source%/float_to_real_conversions/float_to_rea
 ghdl -a --ieee=synopsys --std=08 %source%/float_arithmetic_operations/float_arithmetic_operations_pkg.vhd
 ghdl -a --ieee=synopsys --std=08 %source%/float_adder/float_adder_pkg.vhd
 ghdl -a --ieee=synopsys --std=08 %source%/float_multiplier/float_multiplier_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/float_to_integer_converter/float_to_integer_converter_pkg.vhd
 
 ghdl -a --ieee=synopsys --std=08 %source%/float_alu/float_alu_pkg.vhd
 
