b4cee86db3e8b40d98f5ffbd2df4fc6c91f84cfd
Jari Honkanen <jaarhonka@yahoo.com>
2024-01-10 06:20:01 +0200

    remove wildcards from vunit_run

--- vunit_run_vhdl_float.py
+++ vunit_run_vhdl_float.py
@@ -44,23 +44,21 @@ mathlib.add_source_files(ROOT / "testbenches/float_to_integer_simulation/*.vhd")
 denormal = VU.add_library("denormal")
 denormal.add_source_files(ROOT / "float_type_definitions/float_word_length_20_bit_pkg.vhd")
 denormal.add_source_files(ROOT / "float_type_definitions/float_type_definitions_pkg.vhd")
-denormal.add_source_files(ROOT / "float_arithmetic_operations/*.vhd")
+denormal.add_source_files(ROOT / "float_arithmetic_operations/float_arithmetic_operations_pkg.vhd")
 
-denormal.add_source_files(ROOT / "normalizer/normalizer_configuration/normalizer_with_2_stage_pipe_pkg.vhd")
-denormal.add_source_files(ROOT / "normalizer/*.vhd")
+denormal.add_source_files(ROOT / "normalizer/normalizer_configuration/normalizer_with_1_stage_pipe_pkg.vhd")
+denormal.add_source_files(ROOT / "normalizer/normalizer_pkg.vhd")
 
-denormal.add_source_files(ROOT / "denormalizer/denormalizer_configuration/denormalizer_with_2_stage_pipe_pkg.vhd")
-denormal.add_source_files(ROOT / "denormalizer/*.vhd")
+denormal.add_source_files(ROOT / "denormalizer/denormalizer_configuration/denormalizer_with_1_stage_pipe_pkg.vhd")
+denormal.add_source_files(ROOT / "denormalizer/denormalizer_pkg.vhd")
 
-denormal.add_source_files(ROOT / "float_to_real_conversions" / "*.vhd")
+denormal.add_source_files(ROOT / "float_to_real_conversions/float_to_real_functions_pkg.vhd")
+denormal.add_source_files(ROOT / "float_to_real_conversions/float_to_real_conversions_pkg.vhd")
 
-denormal.add_source_files(ROOT / "float_adder/*.vhd")
-
-denormal.add_source_files(ROOT / "float_multiplier/*.vhd")
-
-denormal.add_source_files(ROOT / "float_alu/*.vhd")
-
-denormal.add_source_files(ROOT / "float_first_order_filter/*.vhd")
+denormal.add_source_files(ROOT / "float_adder/float_adder_pkg.vhd")
+denormal.add_source_files(ROOT / "float_multiplier/float_multiplier_pkg.vhd")
+denormal.add_source_files(ROOT / "float_alu/float_alu_pkg.vhd")
+denormal.add_source_files(ROOT / "float_first_order_filter/float_first_order_filter_pkg.vhd")
 
 denormal.add_source_files(ROOT / "testbenches/simulate_normalizer/*.vhd")
 denormal.add_source_files(ROOT / "testbenches/denormalizer_simulation/*.vhd")
