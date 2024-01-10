875e27c28a551089c23759a87dfe7da9b7e932ad
Jari Honkanen <jaarhonka@yahoo.com>
2024-01-10 06:43:39 +0200

    add float to real conversion to tb_enormal_numbers

---
+++ testbenches/denormalized_numbers/tb_denormal_conversions.vhd
@@ -0,0 +1,54 @@
+LIBRARY ieee  ; 
+    USE ieee.NUMERIC_STD.all  ; 
+    USE ieee.std_logic_1164.all  ; 
+    use ieee.math_real.all;
+
+library vunit_lib;
+context vunit_lib.vunit_context;
+
+    use work.float_to_real_conversions_pkg.all;
+
+entity denormal_conversions_tb is
+  generic (runner_cfg : string);
+end;
+
+architecture vunit_simulation of denormal_conversions_tb is
+
+    constant clock_period      : time    := 1 ns;
+    constant simtime_in_clocks : integer := 50;
+    
+    signal simulator_clock     : std_logic := '0';
+    signal simulation_counter  : natural   := 0;
+    -----------------------------------
+    -- simulation specific signals ----
+
+    constant test_result  : real := 0.0055435133453;
+    constant test         : real := to_real(to_float(to_std_logic_vector(to_float(test_result))));
+    signal should_be_zero : real := test_result - test;
+    signal testi          : real := test;
+
+begin
+
+------------------------------------------------------------------------
+    simtime : process
+    begin
+        test_runner_setup(runner, runner_cfg);
+        wait for simtime_in_clocks*clock_period;
+        test_runner_cleanup(runner); -- Simulation ends here
+        wait;
+    end process simtime;	
+
+    simulator_clock <= not simulator_clock after clock_period/2.0;
+------------------------------------------------------------------------
+
+    stimulus : process(simulator_clock)
+
+    begin
+        if rising_edge(simulator_clock) then
+            simulation_counter <= simulation_counter + 1;
+
+
+        end if; -- rising_edge
+    end process stimulus;	
+------------------------------------------------------------------------
+end vunit_simulation;
