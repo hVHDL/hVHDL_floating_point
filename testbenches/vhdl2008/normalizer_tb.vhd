12620f87664cd3d7dfa158ae1905bf21874e147d
Jari Honkanen <jaarhonka@yahoo.com>
2025-08-31 10:50:14 +0300

    add check to normalizer_tb also

--- testbenches/vhdl2008/normalizer_tb.vhd
+++ testbenches/vhdl2008/normalizer_tb.vhd
@@ -5,7 +5,7 @@ LIBRARY ieee  ;
     use ieee.math_real.all;
     
 library vunit_lib;
-    use vunit_lib.run_pkg.all;
+    context vunit_lib.vunit_context;
 
 entity tb_normalizer is
   generic (runner_cfg : string);
@@ -34,9 +34,7 @@ architecture vunit_simulation of tb_normalizer is
 
     constant float_zero : float_record :=(sign => '0', exponent => (7 downto 0 => x"00"), mantissa => (23 downto 0 => x"000000"));
 
-    constant init_normalizer : normalizer_record := (
-        normalizer_is_requested => "00"
-        ,normalized_data => (1 downto 0 => float_zero));
+    constant init_normalizer : normalizer_record := normalizer_typeref(2, floatref => float_zero);
 
     signal normalizer : init_normalizer'subtype := init_normalizer;
     signal conv_result : float_zero'subtype := float_zero;
@@ -53,6 +51,7 @@ begin
         test_runner_setup(runner, runner_cfg);
         simulation_running <= true;
         wait for simtime_in_clocks*clock_per;
+        check(convref = float32_conv_result, "conversions did not match ");
         simulation_running <= false;
         test_runner_cleanup(runner); -- Simulation ends here
         wait;
